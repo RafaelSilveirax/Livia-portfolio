1fb63b3001e243c7f5dcbdac88f5680e98bf243a
RafaelSilveirax <44031853+RafaelSilveirax@users.noreply.github.com>
2026-04-12 21:53:21 -0300

    feat: titulo contact

--- src/sections/contact/Contact.tsx
+++ src/sections/contact/Contact.tsx
@@ -7,12 +7,10 @@ function Contact() {
       className="bg-[color-mix(in_srgb,var(--color-livia-turquoise)_12%,white)] text-foreground flex  justify-center py-20"
     >
       <div className="w-full max-w-[1200px] mx-auto px-6">
-        <div className="flex justify-center gap-3 mb-6">
-          <h2 className="font-playfair font-bold text-[clamp(2.8rem,6vw,5rem)] leading-[1.05] mb-14">
-            Vamos <em className="not-italic text-livia-turquoise">trabalhar</em>{" "}
-            juntos?
-          </h2>
-        </div>
+        <h2 className="font-playfair font-bold text-[clamp(2rem,3vw,3rem)] text-livia-navy-blue mb-14 text-center">
+          Vamos
+          <em className="not-italic text-livia-turquoise">trabalhar</em> juntos?
+        </h2>
 
         <div className="grid grid-cols-2 gap-16 max-[900px]:grid-cols-1 max-[900px]:gap-10">
           <ContactInfo />
